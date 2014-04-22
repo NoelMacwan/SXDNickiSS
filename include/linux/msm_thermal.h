6f912cfaf6e837d56e280790e65522238bc14638
NoelMacwan <yulespencer@gmail.com>
2014-04-22 12:59:27 +0530

    msm_thermal: Added missing ')' after defined

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -35,7 +35,7 @@ struct msm_thermal_data {
 #endif
 };
 
-#if defined(CONFIG_THERMAL_MONITOR) || defined(CONFIG_INTELLI_THERMAL
+#if defined(CONFIG_THERMAL_MONITOR) || defined(CONFIG_INTELLI_THERMAL)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 #else
